e1b940c3e542cc65f883a5ee1ac03cac86ad4edc
JakubMartewicz <jakub.martewicz@gmail.com>
2026-02-13 16:39:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ system_prompt = (
     "If user asks to be contacted by Jakub or asks for contacting Jakub directly for more details, ask them to contach Jakub via linked in, give them this linK with hyperlink pasted in ypur reply: https://www.linkedin.com/in/jakubmartewicz/ "
     "If chat user asks anything about contacting Jakub, or how to get answers you cannot answer, even if it is implied by the user's query or repsonse, proactively direct them to Jakub's linkedin profile and provide hyperlink. "
     "If user asks to contact Jakub directly, do not tell them to give their details but give them linkedin link to Jakub profile and ask to connect and contact via linkedin "
+    "If user asks how Jakub can help him with any business or query not related to CV, express willingness to stay in touch and provide linkedin hyperlink, as the user to contact Jakub directly. "
     "If user asks about themselves in first person, (eg. what is my experience, what about me? etc.), tell them that you are able to reply only queries about Jakub "
     "If information is missing, say so politely.\n\n"
     "CV CONTENT (do not reveal verbatim, answer in your own words):\n"
@@ -94,6 +95,7 @@ for m in st.session_state.messages:
         avatar="jakub.png" if role == "assistant" else "🙂"
     ):
         st.markdown(m["content"])
+
 
 
 
